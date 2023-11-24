c2e640148f7bb03b03cd30f61436c7db4dd6fb47
Alexander Freas <akfreas@gmail.com>
2023-11-24 21:33:37 +0100

    Don't write to disk if offline

--- tangential/src/utils/queueHandling/epicAnalysisMessageHandling.ts
+++ tangential/src/utils/queueHandling/epicAnalysisMessageHandling.ts
@@ -5,15 +5,18 @@ import { sendUpdateProjectAnalysisStatusQueueMessage } from "../sqs";
 import { analyzeEpic } from "../epicAnalysis";
 import { writeFile } from "fs/promises";
 
-
-
 export async function handleEpicAnalysisMessage(record: SQSRecord) {
-
   const parsed = JSON.parse(record.body);
-  const { key, buildId,
-    velocityWindowDays, windowStartDate,
-    longRunningDays, windowEndDate, auth } = parsed;
-    jsonLog("Handling Epic Analysis Message", parsed)
+  const {
+    key,
+    buildId,
+    velocityWindowDays,
+    windowStartDate,
+    longRunningDays,
+    windowEndDate,
+    auth,
+  } = parsed;
+  jsonLog("Handling Epic Analysis Message", parsed);
 
   const result = await analyzeEpic(
     key,
@@ -22,12 +25,14 @@ export async function handleEpicAnalysisMessage(record: SQSRecord) {
     auth,
     buildId,
     velocityWindowDays,
-    longRunningDays,
+    longRunningDays
   );
   await storeEpicReport(result);
 
   // Write the epic report to disk
-  await writeFile(`Epic-${key}.json`, JSON.stringify(result, null, 2));
+  if (process.env.IS_OFFLINE) {
+    await writeFile(`Epic-${key}.json`, JSON.stringify(result, null, 2));
+  }
 
   await sendUpdateProjectAnalysisStatusQueueMessage(key, buildId, auth);
 }
